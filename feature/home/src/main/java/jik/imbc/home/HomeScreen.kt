fcbc58bafdc64e7532423e6286869411ce3fc3b6
heejik <heejik@imbc.co.kr>
2025-11-24 16:44:27 +0900

    HomeScreen 드라마, 예능 리스트 구현

--- feature/home/src/main/java/jik/imbc/home/HomeScreen.kt
+++ feature/home/src/main/java/jik/imbc/home/HomeScreen.kt
@@ -1,11 +1,25 @@
 package jik.imbc.home
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jik.imbc.designsystem.state.EmptyLoading
+import jik.imbc.home.component.ContentCard
 import jik.imbc.home.model.HomeUiState
+import jik.imbc.model.Content
 
 @Composable
 fun HomeRoute(
@@ -13,6 +27,7 @@ fun HomeRoute(
 ) {
     val viewModel: HomeViewModel = viewModel()
     val uiState: HomeUiState = viewModel.uiState.collectAsStateWithLifecycle().value
+    val context = LocalContext.current
 
     when (uiState) {
         is HomeUiState.Loading -> {
@@ -23,7 +38,9 @@ fun HomeRoute(
             HomeScreen(
                 modifier = modifier,
                 homeUiState = uiState,
-                onClickContent = { contentId -> }
+                onClickContent = { contentId ->
+                    Toast.makeText(context, "contentId: $contentId", Toast.LENGTH_SHORT).show()
+                }
             )
         }
     }
@@ -34,7 +51,50 @@ fun HomeRoute(
 internal fun HomeScreen(
     modifier: Modifier = Modifier,
     homeUiState: HomeUiState.Success,
-    onClickContent: (id: String) -> Unit
+    onClickContent: (id: Int) -> Unit
 ) {
 
+    Column(
+        modifier = modifier
+    ) {
+        ContentList(
+            contents = homeUiState.dramas,
+            onClickContent = onClickContent
+        )
+        ContentList(
+            contents = homeUiState.entertainments,
+            onClickContent = onClickContent
+        )
+    }
+}
+
+@Composable
+internal fun ContentList(
+    modifier: Modifier = Modifier,
+    contents: List<Content>,
+    onClickContent: (id: Int) -> Unit
+) {
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(14.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        items(items = contents, key = { it.id }) { content ->
+            ContentCard(
+                content = content,
+                onClick = { onClickContent(content.id) }
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen(
+        homeUiState = HomeUiState.Success(dramas = emptyList(), entertainments = emptyList()),
+        onClickContent = {}
+    )
 }
