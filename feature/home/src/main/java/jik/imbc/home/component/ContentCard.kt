0d5668dc60141aed452d1af5620ce97a08b7ec35
heejik <heejik@imbc.co.kr>
2025-11-24 10:33:24 +0900

    core:ui 모듈 생성

---
+++ feature/home/src/main/java/jik/imbc/home/component/ContentCard.kt
@@ -0,0 +1,10 @@
+package jik.imbc.home.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun ContentCard(
+    modifier: Modifier = Modifier
+) {
+}
