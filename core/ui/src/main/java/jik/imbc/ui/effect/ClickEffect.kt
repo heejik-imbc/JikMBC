5b35ce7bef96a03069b130cbc51a70d8d7e9ef1a
heejik <heejik@imbc.co.kr>
2025-11-24 16:44:01 +0900

    ClickEffect 구현

---
+++ core/ui/src/main/java/jik/imbc/ui/effect/ClickEffect.kt
@@ -0,0 +1,45 @@
+package jik.imbc.ui.effect
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.input.pointer.pointerInput
+
+@Composable
+fun Modifier.onClickWithPressEffect(
+    scaleFactor: Float = 0.7f,
+    alphaFactor: Float = 0.7f,
+    onClick: () -> Unit
+): Modifier = composed {
+    var isPressed by remember { mutableStateOf(false) }
+
+    val animatedScale by animateFloatAsState(
+        targetValue = if (isPressed) scaleFactor else 1f
+    )
+
+    val animatedAlpha by animateFloatAsState(
+        targetValue = if (isPressed) alphaFactor else 1f
+    )
+
+    this
+        .scale(animatedScale)
+        .alpha(animatedAlpha)
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onPress = {
+                    isPressed = true
+                    val released = tryAwaitRelease()
+                    isPressed = false
+                    if (released) onClick.invoke()
+                },
+            )
+        }
+}
