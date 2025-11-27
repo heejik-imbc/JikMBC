f8a2a82503326e030fb23924c6d39fc3d4c237d4
heejik <heejik@imbc.co.kr>
2025-11-27 15:13:44 +0900

    DetailScreen Rating 칩 추가

---
+++ core/designsystem/src/main/java/jik/imbc/designsystem/component/Chip.kt
@@ -0,0 +1,28 @@
+package jik.imbc.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun JbcChip(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.05f))
+            .padding(8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        content()
+    }
+}
--- feature/detail/src/main/java/jik/imbc/detail/DetailScreen.kt
+++ feature/detail/src/main/java/jik/imbc/detail/DetailScreen.kt
@@ -4,16 +4,31 @@ import android.widget.Toast
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import jik.imbc.data.mock.MockDramas
+import jik.imbc.designsystem.component.JbcChip
+import jik.imbc.designsystem.icon.JbcIcons
 import jik.imbc.designsystem.state.EmptyLoading
 import jik.imbc.detail.component.DetailTopBar
 import jik.imbc.detail.model.DetailUiState
@@ -68,6 +83,18 @@ private fun DetailScreen(
                 imageUrl = uiState.content.thumbnailUrl,
                 description = uiState.content.description
             )
+            Spacer(modifier = Modifier.height(8.dp))
+            Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+                MainInfo(
+                    title = uiState.content.title,
+                    rating = uiState.content.rating.toDouble(),
+                    ratingCount = uiState.content.ratingCount,
+                    releaseYear = uiState.content.releaseYear
+                )
+                Description(
+                    description = uiState.content.description
+                )
+            }
         }
     }
 }
@@ -99,6 +126,61 @@ private fun SharedTransitionScope.Thumbnail(
         contentDescription = description,
         contentScale = ContentScale.Fit
     )
+}
+
+@Composable
+private fun MainInfo(
+    modifier: Modifier = Modifier,
+    title: String,
+    rating: Double,
+    ratingCount: Int,
+    releaseYear: String
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            RatingChip(
+                rating = rating,
+                ratingCount = ratingCount
+            )
+        }
+    }
+}
+
+@Composable
+private fun RatingChip(
+    modifier: Modifier = Modifier,
+    rating: Double,
+    ratingCount: Int
+) {
+    JbcChip(modifier = modifier) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                modifier = Modifier.size(20.dp),
+                imageVector = JbcIcons.Star,
+                contentDescription = "Rating",
+                tint = Color(0xFFFFD250),
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "$rating", fontSize = 13.sp)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "($ratingCount)", fontSize = 13.sp, color = Color(0xFF65A1EC))
+        }
+    }
+}
+
+
+@Composable
+private fun Description(
+    modifier: Modifier = Modifier,
+    description: String
+) {
+
 }
 
 
