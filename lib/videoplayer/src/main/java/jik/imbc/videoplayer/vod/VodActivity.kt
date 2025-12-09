aaa5dfe78d5fd15ad7507b8a3f594b240b14d197
heejik <heejik@imbc.co.kr>
2025-12-09 11:41:39 +0900

    상세 화면 -> Vod Player 이동 로직 구현

--- lib/videoplayer/src/main/java/jik/imbc/videoplayer/vod/VodActivity.kt
+++ lib/videoplayer/src/main/java/jik/imbc/videoplayer/vod/VodActivity.kt
@@ -1,5 +1,7 @@
 package jik.imbc.videoplayer.vod
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +16,16 @@ class VodActivity : ComponentActivity() {
         setContent {
             JikMBCTheme {
                 VodRoute()
+            }
+        }
+    }
+
+    companion object {
+        internal const val EXTRA_CONTENT_ID = "EXTRA_CONTENT_ID"
+
+        fun newIntent(context: Context, contentId: Int): Intent {
+            return Intent(context, VodActivity::class.java).apply {
+                putExtra(EXTRA_CONTENT_ID, contentId)
             }
         }
     }
--- lib/videoplayer/src/main/java/jik/imbc/videoplayer/vod/VodViewModel.kt
+++ lib/videoplayer/src/main/java/jik/imbc/videoplayer/vod/VodViewModel.kt
@@ -1,12 +1,22 @@
 package jik.imbc.videoplayer.vod
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
 import jik.imbc.videoplayer.player.vod.VodPlayer
+import jik.imbc.videoplayer.vod.VodActivity.Companion.EXTRA_CONTENT_ID
 
-class VodViewModel(application: Application) : AndroidViewModel(application = application) {
+class VodViewModel(
+    application: Application,
+    savedStateHandle: SavedStateHandle
+) : AndroidViewModel(application = application) {
 
     val player: VodPlayer = VodPlayer(context = application)
 
+    val contentId: Int = requireNotNull(savedStateHandle.get<Int>(EXTRA_CONTENT_ID))
 
+    init {
+        Log.d("heejik", "contentId: $contentId")
+    }
 }
