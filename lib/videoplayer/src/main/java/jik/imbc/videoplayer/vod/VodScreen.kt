2adb87de2b31f79d5de5535c81f9e6aa35086f80
heejik <heejik@imbc.co.kr>
2025-12-11 16:56:42 +0900

    VOD 화면 상단 Controller 구현 및 Back 버튼 기능 추가

--- lib/videoplayer/src/main/java/jik/imbc/videoplayer/vod/VodScreen.kt
+++ lib/videoplayer/src/main/java/jik/imbc/videoplayer/vod/VodScreen.kt
@@ -1,5 +1,6 @@
 package jik.imbc.videoplayer.vod
 
+import androidx.activity.compose.LocalActivity
 import androidx.annotation.OptIn
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.TweenSpec
@@ -13,9 +14,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,6 +32,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.common.util.UnstableApi
@@ -35,6 +42,7 @@ import androidx.media3.ui.compose.PlayerSurface
 import androidx.media3.ui.compose.SURFACE_TYPE_SURFACE_VIEW
 import androidx.media3.ui.compose.modifiers.resizeWithContentScale
 import androidx.media3.ui.compose.state.rememberPresentationState
+import jik.imbc.designsystem.icon.JbcIcons.ArrowBack
 import jik.imbc.ui.layout.noRippleClickable
 import jik.imbc.videoplayer.R
 import jik.imbc.videoplayer.icons.VideoPlayerIcons
@@ -51,6 +59,7 @@ internal fun VodRoute(
     viewModel: VodViewModel = viewModel()
 ) {
     val uiState: VodUiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val activity = LocalActivity.current
 
     VodScreen(
         modifier = modifier
@@ -58,11 +67,13 @@ internal fun VodRoute(
             .background(color = Color.Black),
         player = viewModel.player.player,
         playerState = uiState.playerState,
+        title = uiState.content.title,
         position = uiState.position,
         duration = uiState.duration,
         onBackward = viewModel::skipBack,
         onPlayPauseReplay = viewModel::playPauseReplay,
-        onForward = viewModel::skipForward
+        onForward = viewModel::skipForward,
+        navigateUp = { activity?.finish() }
     )
 }
 
@@ -71,11 +82,13 @@ private fun VodScreen(
     modifier: Modifier = Modifier,
     player: ExoPlayer,
     playerState: VodPlayerState,
+    title: String,
     position: Long,
     duration: Long,
     onBackward: () -> Unit,
     onPlayPauseReplay: () -> Unit,
     onForward: () -> Unit,
+    navigateUp: () -> Unit
 ) {
 
     var controllerVisible by remember { mutableStateOf(true) }
@@ -97,12 +110,13 @@ private fun VodScreen(
                 VodController(
                     visible = controllerVisible,
                     playerState = playerState,
+                    title = title,
                     position = position,
                     duration = duration,
                     onBackward = onBackward,
                     onPlayPauseReplay = onPlayPauseReplay,
                     onForward = onForward,
-                    navigateUp = {}
+                    navigateUp = navigateUp
                 )
             }
         }
@@ -143,6 +157,7 @@ private fun VodController(
     modifier: Modifier = Modifier,
     visible: Boolean,
     playerState: ActiveState,
+    title: String,
     position: Long,
     duration: Long,
     onBackward: () -> Unit,
@@ -164,6 +179,7 @@ private fun VodController(
         ) {
             VodTopController(
                 modifier = Modifier.weight(1f),
+                title = title,
                 navigateUp = navigateUp
             )
             VodCenterController(
@@ -185,9 +201,31 @@ private fun VodController(
 @Composable
 private fun VodTopController(
     modifier: Modifier = Modifier,
+    title: String,
     navigateUp: () -> Unit
 ) {
-
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 12.dp)
+    ) {
+        Icon(
+            modifier = Modifier
+                .padding(start = 24.dp)
+                .size(32.dp)
+                .align(Alignment.TopStart)
+                .noRippleClickable(onClick = navigateUp),
+            imageVector = ArrowBack,
+            contentDescription = "Back",
+            tint = Color.White,
+        )
+        Text(
+            modifier = Modifier.align(Alignment.TopCenter),
+            text = title,
+            color = Color.White,
+            fontSize = 14.sp
+        )
+    }
 }
 
 
@@ -261,7 +299,7 @@ private fun VodBottomController(
     position: Long,
     duration: Long
 ) {
-
+    Box(modifier = modifier) {}
 }
 
 @Preview
